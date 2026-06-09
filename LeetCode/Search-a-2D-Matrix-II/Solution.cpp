b025f933b976bbf7709cb595bf2e9c4798e4f14d
abhijeetsrivastava662-hue <abhijeetsrivastava662@gmail.com>
2026-06-09 20:15:00 +0530

    Add solution for Search a 2D Matrix II

--- LeetCode/Search-a-2D-Matrix-II/Solution.cpp
+++ LeetCode/Search-a-2D-Matrix-II/Solution.cpp
@@ -1,23 +1,15 @@
 1class Solution {
 2public:
-3    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-4        int row = matrix.size();
-5        int col = matrix[0].size();
-6        int rowIndex=0;
-7        int colIndex=col-1;
-8
-9        while(rowIndex<row && colIndex>=0){
-10            int element = matrix[rowIndex][colIndex];
-11
-12            if(element==target){
-13                return 1;
-14            }
-15            if(element<target){
-16                rowIndex++;
-17            }else{
-18                colIndex--;
-19            }
-20        }
-21        return 0;    
-22    }
-23};
+3    bool searchMatrix(vector<vector<int>>& mat, int target) {
+4        int n=mat.size();
+5        int m=mat[0].size();
+6        int row=0,col=m-1;
+7        while(row<n && col>=0){
+8            if(mat[row][col]==target)
+9            return true;
+10            else if(mat[row][col]<target) row++;
+11            else col--;
+12        }
+13        return false;
+14    }
+15};
