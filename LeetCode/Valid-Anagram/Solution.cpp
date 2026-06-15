5c3a68a9f1777e99d0bb7bb7c987ca4bd40020c5
abhijeetsrivastava662-hue <abhijeetsrivastava662@gmail.com>
2026-06-15 22:01:48 +0530

    Add solution for Valid Anagram

--- LeetCode/Valid-Anagram/Solution.cpp
+++ LeetCode/Valid-Anagram/Solution.cpp
@@ -1,16 +1,23 @@
 1class Solution {
 2public:
 3    bool isAnagram(string s, string t) {
-4        std::unordered_map<char,int> ma;
-5        std::unordered_map<char,int> mb;
-6
-7        for(const auto&el: s){
-8            ma[el]++;
-9        }
-10        for(const auto&el: t){
-11            mb[el]++;
-12        }
-13        return ma==mb;
-14        
-15    }
-16};
+4        if(s.size()!=t.size()) return false;
+5
+6        vector<int> freq(26,0); // ek nayi array banao talol possible appering characters ke
+7
+8        for(char ch : s){
+9           freq[ch-'a']++; //array mein increment kardo agar vo element s mein ho toh
+10        }
+11        for(char ch : t){
+12            freq[ch-'a']--; //array mein decrement agar vo element t mein ho toh
+13        }
+14
+15        for(int count : freq){
+16            if(count!=0)
+17                return false;//matlab koi toh element jyada ya different hai kisi string mein
+18            
+19        }
+20        return true;
+21        
+22    }
+23};
