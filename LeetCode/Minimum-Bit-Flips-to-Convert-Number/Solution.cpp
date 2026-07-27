6ce7e7038a2d585d42479e724196df91c9d58ce1
abhijeetsrivastava662-hue <abhijeetsrivastava662@gmail.com>
2026-07-27 22:19:40 +0530

    Add solution for Minimum Bit Flips to Convert Number

--- LeetCode/Minimum-Bit-Flips-to-Convert-Number/Solution.cpp
+++ LeetCode/Minimum-Bit-Flips-to-Convert-Number/Solution.cpp
@@ -1,16 +1,13 @@
-987654321
-123456789
-170
-85
-16777215
-536870911
-536870911
-0
-536868863
-2048
-1000000000
-1
-0
-0
-8
-15
+1class Solution {
+2public:
+3    int minBitFlips(int start, int goal) {
+4        int ans =start^goal;
+5        int cnt=0;
+6        for(int i=0;i<31;i++){
+7            if(ans & (1<<i))
+8            cnt++;
+9        }
+10        return cnt;
+11        
+12    }
+13};
