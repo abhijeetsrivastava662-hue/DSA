40d9cf31e89687ed514cb73adbc7149ce5d39ef4
abhijeetsrivastava662-hue <abhijeetsrivastava662@gmail.com>
2026-08-06 12:15:39 +0530

    Add solution for Concatenate Array With Reverse

--- LeetCode/Concatenate-Array-With-Reverse/Solution.cpp
+++ LeetCode/Concatenate-Array-With-Reverse/Solution.cpp
@@ -5,11 +5,12 @@
 5        for(int x:nums){
 6        ans.push_back(x);
 7        }
-8        reverse(nums.begin(),nums.end());
-9        for(int x:nums){
-10        ans.push_back(x);
-11        }
-12        return ans;
-13        
-14    }
-15};
+8        //reverse(nums.begin(),nums.end());
+9        if(nums.empty()) return ans;
+10        for(int i=nums.size()-1;i>=0;i--){
+11        ans.push_back(nums[i]);
+12        }
+13        return ans;
+14        
+15    }
+16};
