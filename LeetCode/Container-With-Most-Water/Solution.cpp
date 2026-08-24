0678a96fde3ba49d4ad013f45c951c0dec0f98c5
abhijeetsrivastava662-hue <abhijeetsrivastava662@gmail.com>
2026-08-24 11:42:08 +0530

    Add solution for Container With Most Water

--- LeetCode/Container-With-Most-Water/Solution.cpp
+++ LeetCode/Container-With-Most-Water/Solution.cpp
@@ -4,17 +4,22 @@
 4        int n=height.size();
 5        int l=0;
 6        int r=n-1;
-7        int ans=0;
-8        while(l<r){
-9            ans= max(ans,(r-l)*min(height[l],height[r]));
-10            if(height[l]<height[r]){
-11                l++;
-12            }else{
-13                r--;
-14            }
-15
-16        }
-17        return ans;
-18
-19    }
-20};
+7        int maxi=0;
+8
+9        while(l<r){
+10            if(height[l]<=height[r]){
+11                int area = min(height[l],height[r])*(r-l);
+12                maxi=max(maxi,area);
+13                l++;
+14
+15            }else {
+16                int area = min(height[l],height[r])*(r-l);
+17                maxi=max(maxi,area);
+18                r--;
+19            }
+20
+21        }
+22        return maxi;
+23
+24    }
+25};
