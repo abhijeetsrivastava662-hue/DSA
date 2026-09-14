178115a145ad793f86ffd0402d2c40df0d6ea7e3
abhijeetsrivastava662-hue <abhijeetsrivastava662@gmail.com>
2026-09-14 07:16:24 +0530

    Add solution for Rectangle Overlap

--- LeetCode/Rectangle-Overlap/Solution.cpp
+++ LeetCode/Rectangle-Overlap/Solution.cpp
@@ -1,1 +1,8 @@
-if (x1 >= x4 || y1 >= y4 || ...) {}
+1class Solution {
+2public:
+3    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
+4        if(rec1[2]<=rec2[0] || rec2[2]<=rec1[0] || rec1[1]>=rec2[3] || rec1[3]<=rec2[1]) return false;
+5        else 
+6        return true;
+7    }
+8};
