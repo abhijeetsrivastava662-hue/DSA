e4241a4ffc245a4652026fa312ecbc90671ba5d1
abhijeetsrivastava662-hue <abhijeetsrivastava662@gmail.com>
2026-09-15 13:55:13 +0530

    Add solution for Single Element in a Sorted Array

--- LeetCode/Single-Element-in-a-Sorted-Array/Solution.cpp
+++ LeetCode/Single-Element-in-a-Sorted-Array/Solution.cpp
@@ -1,23 +1,22 @@
 1class Solution {
 2public:
 3    int singleNonDuplicate(vector<int>& nums) {
-4        int n= nums.size();
+4        int n=nums.size();
 5        if(n==1) return nums[0];
 6        if(nums[0]!=nums[1]) return nums[0];
 7        if(nums[n-1]!=nums[n-2]) return nums[n-1];
-8        int low=1,high = n-2;
-9        while(low<=high){
-10            int mid=low+(high-low)/2;
-11            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) return nums[mid];
-12            else //standing on left half
-13            if((mid%2==1 && nums[mid]==nums[mid-1]) || (mid%2==0 && nums[mid]==nums[mid+1]))
-14            {   // eliminate left half
+8        int low=0;
+9        int high=n-2;
+10        while(low<=high){
+11            int mid=low+(high-low)/2;
+12            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
+13                return nums[mid];
+14            }else if((mid%2==1 && nums[mid]==nums[mid-1]) || (mid%2==0 && nums[mid]==nums[mid+1])){
 15                low=mid+1;
-16            }else //standing on left half 
-17            {   // eliminate right half
-18                high=mid-1;
-19            }
-20        }
-21        return -1;
-22    }
-23};
+16            }else{
+17                high=mid-1;
+18            }
+19        }
+20        return -1;
+21    }
+22};
