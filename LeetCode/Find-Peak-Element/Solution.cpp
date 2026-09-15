4684759d017dd656bb125bb718dcc0379676ea7e
abhijeetsrivastava662-hue <abhijeetsrivastava662@gmail.com>
2026-09-15 14:09:10 +0530

    Add solution for Find Peak Element

--- LeetCode/Find-Peak-Element/Solution.cpp
+++ LeetCode/Find-Peak-Element/Solution.cpp
@@ -3,21 +3,20 @@
 3    int findPeakElement(vector<int>& nums) {
 4        int n=nums.size();
 5        if(n==1) return 0;
-6        if(nums[0]>nums[1]) return 0;//first element can be peak after satisfying this condition
-7        if(nums[n-1]>nums[n-2]) return n-1;// last element can be peak after satisfying this condition
-8        int low=1,high=n-2;
-9        while(low<=high){
-10            int mid=low+(high-low)/2;
-11            //mid is the peak
-12            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]) return mid;
-13            else if(nums[mid]>nums[mid-1])//elements is linearly increasing
-14            {
+6        if(nums[0]>nums[1]) return 0;
+7        if(nums[n-1]>nums[n-2]) return n-1;
+8        int low=1;
+9        int high=nums.size()-2;
+10        while(low<=high){
+11            int mid=(low+high)/2;
+12            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+13                return mid;
+14            }else if(nums[mid]>nums[mid-1]){
 15                low=mid+1;
-16            }else//elements is linearly decreasing and also works if mid comes opposite of peak
-17            {
-18                high=mid-1;
-19            }
-20        }
-21        return -1;
-22    }
-23};
+16            }else{
+17                high=mid-1;
+18            }
+19        }
+20        return -1;
+21    }
+22};
