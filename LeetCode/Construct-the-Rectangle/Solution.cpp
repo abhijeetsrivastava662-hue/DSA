03d7a17dccde312e76e8bb93cfc944f89e7eb772
abhijeetsrivastava662-hue <abhijeetsrivastava662@gmail.com>
2026-09-20 02:47:36 +0530

    Add solution for Construct the Rectangle

---
+++ LeetCode/Construct-the-Rectangle/Solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+        vector<int> constructRectangle(int area) {
+    for (int W = sqrt(area); W >= 1; W--) {
+        if (area % W == 0) {
+            return {area / W, W};
+        }
+    }
+    return {};
+    }
+};
